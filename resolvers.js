8a2aa3bf056f7f2582bc05aa9e3bb38286760c4d
Hermanni Piirainen <>
2018-11-15 01:26:48 +0200

    Add emission resolver, unify population and emission orders (by year)

--- resolvers.js
+++ resolvers.js
@@ -1,5 +1,15 @@
 import axios from 'axios';
 
+const sortByYear = (a, b) => {
+  if (a.date > b.date) {
+    return 1;
+  }
+  if (a.date < b.date) {
+    return -1;
+  }
+  return 0;
+}
+
 const resolvers = {
   Query: {
     allCountries: () => {
@@ -42,7 +52,19 @@ const resolvers = {
           if (response.data.length <= 1) {
             return [];
           }
-          return response.data[1]
+          return response.data[1].filter(population => population.value !== null)
+            .sort(sortByYear);
+        })
+        .catch((error) => console.log(error))
+    },
+    emissions: (country) => {
+      return axios.get(`https://api.worldbank.org/v2/countries/${country.iso2Code}/indicators/EN.ATM.CO2E.KT?format=json&per_page=100`)
+        .then((response) => {
+          if (response.data.length <= 1) {
+            return [];
+          }
+          return response.data[1].filter(emission => emission.value !== null)
+            .sort(sortByYear);
         })
         .catch((error) => console.log(error))
     }
